5360355ed93fffe26470c08cbb5407d62d69b0af
Ysaias Silas Dos Santos Viana <ysaiassilas@gmail.com>
2026-02-10 21:28:08 -0300

    10/02/2026  21:27

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const numeroWhatsApp = "61998020681";
+    const numeroWhatsApp = "61996236584";
 
     // 1. Scroll Suave para as Seções
     const linksInternos = document.querySelectorAll('nav a[href^="#"]');
